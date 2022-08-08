698116fe2cf1ba2810cd7484441449a6cc96e545
Shaun <shaunjiji123@gmail.com>
2022-08-08 16:28:47 +0000

    ageCalculator.js

---
+++ ageCalculator.js
@@ -0,0 +1,8 @@
+const ageCalculator = function (name, yearOfBirth, currentYear) {
+  let age = currentYear - yearOfBirth;
+  console.log(name + " is " + age + " years old.")
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
+
